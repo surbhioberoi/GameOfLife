57a3c7cfe98c754bc3344ef5f1b3a9c8b0361d33
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-05-23 00:03:42 +0530

    code formatting changes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-context.fillStyle = "#FF00AA";
-context.strokeStyle = "gray";
-
 var cells = [];
 var size = 20;
 var columns = canvas.height/size;
 var rows = canvas.width/size;
-
+var interval;
+var refreshPeriod = 200;
+var cellColor = "#FF4165";
+var cellBorderColor = "gray";
 
 
 function updateCells() {
+
 	function _test(x, y) {
 		if (!(cells[x] === undefined) && cells[x][y] === 1) {
 			return 1;
 		}
 		return 0;
-
 	}
 
 	function liveNeighboursCount(x, y) {
@@ -27,8 +27,10 @@ function updateCells() {
 	var newCells = [];
 
 	for (var i = 0; i<rows; i++) {
+
 		newCells[i] = [];
 		for (var j = 0; j<columns; j++) {
+
 			var count = liveNeighboursCount(i, j);
 			if (cells[i][j] ===1 && count < 2) {
 				newCells[i][j] = 0;
@@ -45,14 +47,13 @@ function updateCells() {
 
 		}
 	}
-
 	return newCells;
-
 }
 
 
 function renderGame() {
 	context.clearRect(0,0,canvas.height, canvas.width);
+
 	for (var i = 0; i<rows; i++) {
 		for (var j = 0; j<columns; j++) {
 
@@ -61,11 +62,8 @@ function renderGame() {
 			} else {
 				context.strokeRect(i*size, j*size, size, size);
 			}
-
 		}
 	}
-
-	 
 }
 
 
@@ -74,7 +72,12 @@ function draw() {
 	renderGame();
 }
 
+
 function init() {
+
+	context.fillStyle = cellColor;
+	context.strokeStyle = cellBorderColor;
+
 	for (var i = 0; i<rows; i++) {
 		cells[i] = [];
 		for (var j = 0; j<columns; j++) {
@@ -88,46 +91,37 @@ function init() {
 	renderGame();
 }
 
-init();
-
-
-var interval;
-
-function startGame() {
-	interval = setInterval(draw, 500);
-
-}
-
-
-function stopGame() {
-	clearInterval(interval);
-}
-
-
-function clearGame() {
-	stopGame();
-	init();
-}
 
 var stopButton = document.getElementById("stop");
-stopButton.addEventListener("click", stopGame);
+stopButton.addEventListener("click", function () {
+	clearInterval(interval);
+});
 
 var startButton = document.getElementById("start");
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", function () {
+	interval = setInterval(draw, refreshPeriod);
+
+});
 
 var clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", clearGame);
+clearButton.addEventListener("click", function () {
+	clearInterval(interval);
+	init();
+});
 
-canvas.addEventListener("click", toggleCell);
 
-function toggleCell(event) {
+canvas.addEventListener("click", function (event) {
 
 	var x = Math.floor(event.offsetX/20);
 	var y = Math.floor(event.offsetY/20);
 
 	cells[x][y] = cells[x][y]?0:1;
 	renderGame();
-}
+});
+
+
+init();
+
 
 
 
